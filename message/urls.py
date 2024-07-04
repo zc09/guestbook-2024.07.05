da26808e6deebe91d688926e41cef1c93225583d
xz <eleanor9808@gmail.com>
2024-07-04 16:11:52 +0800

    查看留言 新增留言 登入

--- message/urls.py
+++ message/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from . import views
+from .views import *
 
-urlpatterns = [
+urlpatterns = [ 
     # message/  => 留言列表 
-     path('', views.MassageList.as_view(), name='msg_list')   
-]
+    path('', views.MessageList.as_view(), name='msg_list') 
+    # message/1/, message/2/, ... 
+    path('<int:pk>/', views MessageRead.as_view(), name='msg_view') 
+    path('create/', views.MessageNew.as_view(), name='msg_create')
+    ]
