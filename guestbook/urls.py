7b6bd7ec5dd62ffad79ec8653fca33f568ee7088
xz <eleanor9808@gmail.com>
2024-07-04 16:12:32 +0800

    查看留言 新增留言 登入

--- guestbook/urls.py
+++ guestbook/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('message/', include('message.urls')),
+    path('accounts/',include('django.contrib.auth.urls'))
 ]
--- message/views.py
+++ message/views.py
@@ -1,12 +1,24 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView, CreateView,DeleteView
 from .models import Message
 
 # Create your views here.
 class MassageList(ListView):
     model = Message
 
-    # 應用程式/資料模型_list.html
-    # message/message_list.html
+    # 頁面範本檔案名稱: 應用程式/資料模型_list.html => messsage/message_list.html
+    # 頁面範本變數: 名稱資料模型_list => message_list
 
-    # 資料模型_list
+    class MessageRead(DetailView):
+        model = Message
+
+    # 頁面範本檔案名稱: 應用程式/資料模型_detail.html => messsage/detail_list.html
+    # 頁面範本變數名稱: 名稱資料模型_list => message
+
+class MessageNew(CreateView):
+    model = Message
+    fields = ['user','receipt','subject','content']
+    success_url = '/message/'
+    # fields = '__all__'
+
+     # 頁面範本檔案名稱: 應用程式/資料模型_form.html => messsage/message_form.html
