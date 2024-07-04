9dca4b74ea3603d951ca4c08f02a592195bec8a8
xz <eleanor9808@gmail.com>
2024-07-04 11:14:47 +0800

    定義 Message 資料模型

--- message/models.py
+++ message/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Message(models.Model):
+    user = models.CharField('留言者', max_length=30)
+    receipt = models.CharField('收件人', max_length=60)
+    subject = models.CharField('留言主題',max_length=128)
+    content = models.TextField('留言內容')
+    created = models.DateTimeField('留言時間', auto_now_add=True)
+    updated = models.DateTimeField('最後修改時間', auto_now=True)
