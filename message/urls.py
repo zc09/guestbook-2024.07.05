9575c0244a1ed158270483f5a794267cbaf0ac80
xz <eleanor9808@gmail.com>
2024-07-05 11:20:33 +0800

    限制刪除留言只能登入後台才能操作

--- message/urls.py
+++ message/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
 from .views import *
 
-urlpatterns = [ 
-    # message/  => 留言列表 
-    path('', views.MessageList.as_view(), name='msg_list') 
-    # message/1/, message/2/, ... 
-    path('<int:pk>/', views MessageRead.as_view(), name='msg_view') 
-    path('create/', views.MessageNew.as_view(), name='msg_create')
-    ]
+urlpatterns = [
+    path('', MessageList.as_view(), name='msg_list'), 
+    path('<int:pk>/', MessageView.as_view(), name='msg_view'),
+    path('create/', MessageCreate.as_view(), name='msg_create'),
+    path('<int:pk>/delete/', MessageDelete.as_view(), name='msg_delete'),
+]
